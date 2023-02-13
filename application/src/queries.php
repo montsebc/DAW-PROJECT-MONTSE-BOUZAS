24dd04670c7066bd83f4654f75fbb796d99f3218
mboucas <124385494+mboucas@users.noreply.github.com>
2023-02-13 16:36:51 +0100

    update queries.php

--- application/src/queries.php
+++ application/src/queries.php
@@ -2,20 +2,19 @@
 	include "database.php";
 
     function Login($email, $dni) {
-		
-			$connection = connect();
-			$query = $connection->prepare(
-				"SELECT * FROM usuario WHERE EMAIL = '".$email."' AND DNI = '".$dni."'"
-			);
-			$query->execute();
-			$numeroUsuarios = $query->rowCount();
-			$datos = $query->fetch();
+        $conn = mysqli_connect();
+        $query = $conn->prepare("SELECT * FROM usuario WHERE EMAIL = ? AND DNI = ?");
+        $query->bind_param("ss", $email, $dni);
+        $query->execute();
+        $query->store_result();
+        $numeroUsuarios = $query->num_rows;
+        $datos = $query->fetch();
+        
 
-			if ($numeroUsuarios == 1) {
-					return "usuario";	
-            }
-            else
-				return "usuario no registrado";
-		
-	}
+        if ($numeroUsuarios == 1) { 
+            return "usuario";
+        } else {
+            return "usuario no registrado";
+        }
+    }     
 ?>
